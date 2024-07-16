d345c814783952a7b25cf62e0a7d96bc5cf04441
Warley Roma <warleyroma@gmail.com>
2024-07-16 20:26:37 -0300

    Adicionando Children ao componente layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div>
       <Header />
+      <main>{children}</main>
       <Footer />
     </div>
   );
