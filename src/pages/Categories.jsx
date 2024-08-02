f9d1510314beda9a685ca178029a6082980d57af
Warley Roma <warleyroma@gmail.com>
2024-08-02 12:30:04 -0300

    Exportando component Error404 nas páginas Categories e Myorders

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
 import '../App.css';
-import Section from '../components/Section';
+import Error404 from '../components/Error404';
 
-
-const text = "404 :("
-const text2 = "Oops! Esta página não pode ser encontrada!"
-const text3 = "Parece que não conseguimos encontrar o que você está procurando."
-
-const UnderConstruction = ({text}) => {
-  return (
-    <div>
-      <h3 className="under-construction-title">{text}</h3>
-      <h3 className="under-construction-title2">{text2}</h3>
-      <h3 className="under-construction-title3">{text3}</h3>
-    </div>
-  );
-};
 
 
 
 const Categories = () => {
 
   return (
-    <Section>
-      <UnderConstruction text={text}/>
-  
-    </Section>
+   <Error404></Error404>
   );
 };
 
