6f68c29580a63049b75a86e60ac8cf376b8742ce
Warley Roma <warleyroma@gmail.com>
2024-08-07 10:33:44 -0300

    Atualizando código em ProductListingPage para consumir dados dos produtos vindos de product.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,17 +1,212 @@
 // src/data/products.js
-const productsData = [
+export const productsData = [
   {
-    name: 'Nome do produto 1',
-    image: 'public/product-thumb-1.png',
+    id:1,
+    name: "Tenis Nike Max Ligth Sport",
+    reference: "123456",
+    stars: 4.5,
+    rating: 150,
     price: 200,
-    priceDiscount: 149.9
+    priceDiscount: 149.9,
+    subtitle:"Descrição do produto",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ",
+    subtitle_tam:"Tamanho",
+    subtitle_cor:"Cor",
+    image:"/public/product-thumb-1.jpeg",
+    images_galery: [
+      { src: "/public/produc-image-1.jpeg" },
+      { src: "/public/produc-image-2.jpeg" },
+      { src: "/public/produc-image-3.jpeg" },
+      { src: "/public/produc-image-4.jpeg" },
+      { src: "/public/produc-image-5.jpeg" },
+      
+    ],
   },
+
   {
-    name: 'Nome do produto 2',
-    image: 'public/product-thumb-2.png',
-    price: 49.9
+    id:2,
+    name: "Tenis Nike Air Max Pro",
+    reference: "123456",
+    stars: 4.5,
+    rating: 150,
+    price: 200,
+    priceDiscount: 149.9,
+    subtitle:"Descrição do produto",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ",
+    subtitle_tam:"Tamanho",
+    subtitle_cor:"Cor",
+    image:"/public/product-thumb-2.jpeg",
+    images_galery: [
+      { src: "/public/produc-image-2.jpeg" },
+      { src: "/public/produc-image-1.jpeg" },
+      { src: "/public/produc-image-3.jpeg" },
+      { src: "/public/produc-image-4.jpeg" },
+      { src: "/public/produc-image-5.jpeg" },
+      
+    ],
   },
-  // Adicione mais produtos conforme necessário
+
+  {
+    id:3,
+    name: "Tenis Adidas Superstar",
+    reference: "123456",
+    stars: 4.5,
+    rating: 150,
+    price: 200,
+    priceDiscount: 149.9,
+    subtitle:"Descrição do produto",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ",
+    subtitle_tam:"Tamanho",
+    subtitle_cor:"Cor",
+    image:"/public/product-thumb-3.jpeg",
+    images_galery: [
+      { src: "/public/produc-image-5.jpeg" },
+      { src: "/public/produc-image-2.jpeg" },
+      { src: "/public/produc-image-3.jpeg" },
+      { src: "/public/produc-image-1.jpeg" },
+      { src: "/public/produc-image-5.jpeg" },
+      
+    ],
+  },
+
+  {
+    id:4,
+    name: "Tenis Puma RS-X",
+    reference: "123456",
+    stars: 4.5,
+    rating: 150,
+    price: 200,
+    priceDiscount: 149.9,
+    subtitle:"Descrição do produto",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ",
+    subtitle_tam:"Tamanho",
+    subtitle_cor:"Cor",
+    image:"/public/product-thumb-4.jpeg",
+    images_galery: [
+      { src: "/public/produc-image-4.jpeg" },
+      { src: "/public/produc-image-2.jpeg" },
+      { src: "/public/produc-image-3.jpeg" },
+      { src: "/public/produc-image-1.jpeg" },
+      { src: "/public/produc-image-5.jpeg" },
+      
+    ],
+  },
+
+  {
+    id:5,
+    name: "Tenis New Balance 574",
+    reference: "123456",
+    stars: 4.5,
+    rating: 150,
+    price: 200,
+    priceDiscount: 149.9,
+    subtitle:"Descrição do produto",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ",
+    subtitle_tam:"Tamanho",
+    subtitle_cor:"Cor",
+    image:"/public/product-thumb-5.jpeg",
+    images_galery: [
+      { src: "/public/produc-image-3.jpeg" },
+      { src: "/public/produc-image-2.jpeg" },
+      { src: "/public/produc-image-1.jpeg" },
+      { src: "/public/produc-image-4.jpeg" },
+      { src: "/public/produc-image-5.jpeg" },
+      
+    ],
+  },
+
+  {
+    id:6,
+    name: "Tenis Converse Chuck Taylor",
+    reference: "123456",
+    stars: 4.5,
+    rating: 150,
+    price: 200,
+    priceDiscount: 149.9,
+    subtitle:"Descrição do produto",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ",
+    subtitle_tam:"Tamanho",
+    subtitle_cor:"Cor",
+    image:"/public/product-thumb-4.jpeg",
+    images_galery: [
+      { src: "/public/produc-image-2.jpeg" },
+      { src: "/public/produc-image-1.jpeg" },
+      { src: "/public/produc-image-5.jpeg" },
+      { src: "/public/produc-image-4.jpeg" },
+      { src: "/public/produc-image-3.jpeg" },
+      
+    ],
+  },
+
+  {
+    id:7,
+    name: "Tenis Reebok Classic Leather",
+    reference: "123456",
+    stars: 4.5,
+    rating: 150,
+    price: 200,
+    priceDiscount: 149.9,
+    subtitle:"Descrição do produto",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ",
+    subtitle_tam:"Tamanho",
+    subtitle_cor:"Cor",
+    image:"/public/product-thumb-1.jpeg",
+    images_galery: [
+      { src: "/public/produc-image-3.jpeg" },
+      { src: "/public/produc-image-1.jpeg" },
+      { src: "/public/produc-image-2.jpeg" },
+      { src: "/public/produc-image-5.jpeg" },
+      { src: "/public/produc-image-4.jpeg" },
+      
+    ],
+  },
+
+  {
+    id:8,
+    name: "Tenis Reebok Classic Leather",
+    reference: "123456",
+    stars: 4.5,
+    rating: 150,
+    price: 200,
+    priceDiscount: 149.9,
+    subtitle:"Descrição do produto",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ",
+    subtitle_tam:"Tamanho",
+    subtitle_cor:"Cor",
+    image:"/public/product-thumb-3.jpeg",
+    images_galery: [
+      { src: "/public/produc-image-1.jpeg" },
+      { src: "/public/produc-image-2.jpeg" },
+      { src: "/public/produc-image-3.jpeg" },
+      { src: "/public/produc-image-4.jpeg" },
+      { src: "/public/produc-image-5.jpeg" },
+      
+    ],
+  },
+
+  {
+    id:9,
+    name: "Tenis Asics Gel-Kayano",
+    reference: "123456",
+    stars: 4.5,
+    rating: 150,
+    price: 200,
+    priceDiscount: 149.9,
+    subtitle:"Descrição do produto",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ",
+    subtitle_tam:"Tamanho",
+    subtitle_cor:"Cor",
+    image:"/public/product-thumb-2.jpeg",
+    images_galery: [
+      { src: "/public/produc-image-1.jpeg" },
+      { src: "/public/produc-image-2.jpeg" },
+      { src: "/public/produc-image-3.jpeg" },
+      { src: "/public/produc-image-4.jpeg" },
+      { src: "/public/produc-image-5.jpeg" },
+      
+    ],
+  },
+  
 ];
 
 export default productsData;
--- src/pages/ProductListingPage.jsx
+++ src/pages/ProductListingPage.jsx
@@ -4,26 +4,11 @@ import Section from '../components/Section';
 import ProductListing from '../components/ProductListing';
 import FilterGroup from '../components/FilterGroup';
 import '../App.css';
+import {productsData} from '../data/products';
 
 const ProductListingPage = () => {
   const [sortOrder, setSortOrder] = useState('price-asc');
-  const [products, setProducts] = useState([
-    { id: 1, name: "Nome do produto 1", image: "/public/product-thumb-1.jpeg", price: 200, priceDiscount: 149.9 },
-    { id: 2, name: "Nome do produto 2", image: "/public/product-thumb-2.jpeg", price: 49.9 },
-    { id: 3, name: "Nome do produto 3", image: "/public/product-thumb-3.jpeg", price: 245.9, priceDiscount: 149.9 },
-    { id: 4, name: "Nome do produto 4", image: "/public/product-thumb-4.jpeg", price: 49.9 },
-    { id: 5, name: "Nome do produto 5", image: "/public/product-thumb-5.jpeg", price: 49.9 },
-    { id: 6, name: "Nome do produto 6", image: "/public/product-thumb-4.jpeg", price: 280, priceDiscount: 149.9 },
-    { id: 7, name: "Nome do produto 7", image: "/public/product-thumb-1.jpeg", price: 350.9 },
-    { id: 8, name: "Nome do produto 8", image: "/public/product-thumb-2.jpeg", price: 289, priceDiscount: 149.9 },
-    { id: 9, name: "Nome do produto 9", image: "/public/product-thumb-1.jpeg", price: 49.9 },
-    { id: 10, name: "Nome do produto 10", image: "/public/product-thumb-2.jpeg", price: 49.9 },
-    { id: 11, name: "Nome do produto 11", image: "/public/product-thumb-5.jpeg", price: 255.9, priceDiscount: 149.9 },
-    { id: 12, name: "Nome do produto 12", image: "/public/product-thumb-4.jpeg", price: 49.9 },
-    { id: 13, name: "Nome do produto 13", image: "/public/product-thumb-5.jpeg", price: 49.9 },
-    { id: 14, name: "Nome do produto 14", image: "/public/product-thumb-1.jpeg", price: 350.9, priceDiscount: 149.9 },
-    { id: 15, name: "Nome do produto 15", image: "/public/product-thumb-2.jpeg", price: 49.9 },
-  ]);
+  const [products, setProducts] = useState(productsData);
 
   const handleSortChange = (e) => {
     setSortOrder(e.target.value);
