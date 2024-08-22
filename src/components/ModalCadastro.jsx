eecd508a72b41065db93486942d119586f0966dd
Warley Roma <warleyroma@gmail.com>
2024-08-22 10:07:13 -0300

    Usando fech para requisição POST
    Configurando o form para que seja enviada uma requisição post com os dados do usuário a ser cadastrado utilizando o fech do javascript

--- src/components/ModalCadastro.jsx
+++ src/components/ModalCadastro.jsx
@@ -1,7 +1,31 @@
-import React, { useEffect } from 'react';
-import '../styles/components/ModalCadastro.css'; 
+import React, { useEffect, useState } from 'react';
+import '../styles/components/ModalCadastro.css';
 
 function ModalCadastro({ isOpen, closeModal }) {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const dados = {
+      username,
+      email,
+      password,
+    };
+
+    fetch('http://seu-servidor.com/cadastro', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dados),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  };
   // Adiciona ou remove a classe que desabilita a rolagem
   useEffect(() => {
     if (isOpen) {
@@ -24,19 +48,26 @@ function ModalCadastro({ isOpen, closeModal }) {
         <span className="close" onClick={closeModal}>&times;</span>
         <h2>Cadastre-se</h2>
         <div className='avisolgpd'>
-        <img class="icon" src="https://ae01.alicdn.com/kf/Sa0be8b1ad53f401b8cfb2c5e3bc5373dG/22x26.png" alt="" srcset="" ></img>
+          <img class="icon" src="https://ae01.alicdn.com/kf/Sa0be8b1ad53f401b8cfb2c5e3bc5373dG/22x26.png" alt="" srcset="" ></img>
           <p>Suas informações estão protegidas</p>
-          </div>
-        <form className='form'>
-       
-          <input placeholder="Nome completo" type="text" id="username" name="username" required />
-          
-         
-          <input placeholder="Email" type="email" id="email" name="email" required />
-          
-       
-          <input placeholder="Senha" type="password" id="password" name="password" required />
-          
+        </div>
+        <form className='form' onSubmit={handleSubmit}>
+
+          <input placeholder="Nome completo" type="text" id="username" name="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)} required />
+
+
+          <input placeholder="Email" type="email" id="email" name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)} required />
+
+
+          <input placeholder="Senha" type="password" id="password" name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required />
+
           <button type="submit">Cadastrar</button>
         </form>
       </div>
